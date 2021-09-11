05742749d2e07a0ff682e10ba1396adf8e638098
SHIVAM123P <60048378+SHIVAM123P@users.noreply.github.com>
2021-09-11 22:06:23 +0530

    Final Commit
    Program to find the first and last occurrence of a number in a given array.

---
+++ First_and_last_occurrence_of_a_number_in_an_Array.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int i, int key){
+    if(i==n){
+        return -1;
+    }
+
+    if(arr[i]==key){
+        return i;
+    }
+    return firstOcc(arr,n,i+1,key);
+}
+
+int lastOcc(int arr[], int n, int i, int key){
+    if(i==n){
+        return -1;
+    }
+
+    int restArray = lastOcc(arr,n,i+1,key);
+    if(restArray!=-1){
+        return restArray;
+    }
+    if(arr[i]==key){
+        return i;
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {4,2,1,2,5,2,7};
+    cout<<"First occurrence of the number is on position: "<<firstOcc(arr,7,0,2)<<endl;
+    cout<<"Last occurrence of the number is on position: "<<lastOcc(arr,7,0,2)<<endl; 
+}
